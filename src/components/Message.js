7113451d44a330b03219835f0a868749b3d3a9c3
shivam kumar <classicsivam@gmail.com>
2022-01-19 18:00:08 +0530

    adding-firebase-and-react-components-with-some-css

---
+++ src/components/Message.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { db, auth } from '../firebase'
+import SendMessage from './SendMessage'
+import Button from '@mui/material/Button';
+
+
+function Message() {
+    const scroll = useRef()
+    const [messages, setMessages] = useState([])
+    useEffect(() => {
+        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [])
+    return (
+        <div>
+            <div style={{
+            display: 'flex', justifyContent: 'center', position: 'fixed', width: '100%', backgroundColor: '#FAFAFA', top: 0, borderBottom: 'solid 1px lightgray', zIndex: '10'
+        }}>
+            <Button style={{ padding: '20px', fontSize: '15px', borderRadius: '0', fontWeight: '600' }} onClick={() => auth.signOut()}>Sign Out</Button>
+        </div>
+            <div className="msgs">
+                {messages.map(({ id, text, photoURL, uid }) => (
+                    <div>
+                        <div key={id} className={`msg ${uid === auth.currentUser.uid ? 'sent' : 'received'}`}>
+                            <img src={photoURL} alt="" />
+                            <p>{text}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <SendMessage scroll={scroll} /> 
+             <div ref={scroll}></div>
+        </div>
+    )
+}
+
+export default Message
